845365093c2578a97e64dc3bc4af3ce5a60d96e8
anth <zyle1104@gmail.com>
2024-02-15 18:01:12 +0900

    :sparkles: New: 빈 스코프 까지
    빈 스코프 까지

--- src/test/java/com/example/Core/autowired/AllBeanTest.java
+++ src/test/java/com/example/Core/autowired/AllBeanTest.java
@@ -20,7 +20,11 @@ public class AllBeanTest {
         DiscountService discountService = ac.getBean(DiscountService.class);
         Member member = new Member(1L, "user", Grade.VIP);
         int discountPrice = discountService.discount(member,10000, "fixDiscountPolicy");
-        Assertions.assertThat(discountService).isSameAs(DiscountService.class);
+        Assertions.assertThat(discountService).isInstanceOf(DiscountService.class);
+        Assertions.assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDsicountPrice = discountService.discount(member,20000, "rateDiscountPolicy");
+        Assertions.assertThat(rateDsicountPrice).isEqualTo(2000);
         // TODO 여기서 부터 시작 7분 19초
     }
 
@@ -35,8 +39,9 @@ public class AllBeanTest {
             System.out.println("policyList = " + policyList);
         }
 
-        public int discount(Member member, int i, String fixDiscountPolicy) {
-            return 0;
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member,price);
         }
     }
 }
